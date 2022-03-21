1cd6a488d90d4e36f64419683e59a0a5737b10f1
mak-mat <46903724+mak-mat@users.noreply.github.com>
2022-03-21 20:41:08 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Welcome to my DevOps class";
+        return "Welcome to my DevOps class, Deployment is done successfully ";
     }
 
 }
